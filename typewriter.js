d47822aee5d75b68b2729f8a10f0bdb8ef07ccc1
Brandon <lawlzfactor@live.com>
2015-01-03 19:55:14 -0500

    yeahh

--- typewriter.js
+++ typewriter.js
@@ -3,14 +3,13 @@
     var i = 0,
         text = document.getElementById("text"),
         speak = "Your mother",
-        loop  = function() {
+        loop  = !function() {
           return typeof speak[i] !== 'undefined' ? 
           false : setTimeout(function() {
               text.innerHTML = speak[i];
               i++;
               loop();
           },400)
-            
-        };
+        }();
 
 }(window, document);
